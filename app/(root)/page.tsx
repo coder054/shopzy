74b4bfb617bcb42020e7c5791cf18e8785921e8b
anhdt <tuananh92.ftu@gmail.com>
2025-04-15 12:41:08 +0700

    loading and not found page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
-export default function App() {
+const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+export default async function App() {
+  await delay(2000);
   return (
     <h1 className="border border-red-400 text-green-300">
       Hello world! sraiet nisraet eisrtaen
