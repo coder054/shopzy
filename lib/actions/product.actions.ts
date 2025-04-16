868132292a2b50d05ecd2117f65bbc3b7dda2d1a
anhdt <tuananh92.ftu@gmail.com>
2025-04-16 19:37:26 +0700

    product detail page

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -3,11 +3,19 @@
 import { LATEST_PRODUCTS_LIMIT } from "@/constants";
 import { convertToPlainObject } from "../utils";
 import { prisma } from "@/db/prisma";
+import { equal } from "node:assert";
 
 export async function getLatestProduct() {
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: { createdAt: "desc" },
+  });
+
+  return convertToPlainObject(data);
+}
+export async function getProductBySlug(slug: string) {
+  const data = await prisma.product.findFirst({
+    where: { slug: slug },
   });
 
   return convertToPlainObject(data);
