c26204b3321e6b97a8d811f5f96513a5a69e808c
anhdt <tuananh92.ftu@gmail.com>
2025-05-19 19:46:14 +0700

    display data in admin overview page

--- lib/utils.ts
+++ lib/utils.ts
@@ -131,3 +131,8 @@ export function formUrlQuery({
     { skipNull: true },
   );
 }
+
+const NUMBER_FORMATTER = new Intl.NumberFormat("en-US");
+export function formatNumber(number: number) {
+  return NUMBER_FORMATTER.format(number);
+}
