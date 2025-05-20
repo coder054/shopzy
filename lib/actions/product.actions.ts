837d473c2b862c2b38509814e68c012fffa95ed9
anhdt <tuananh92.ftu@gmail.com>
2025-05-20 10:23:47 +0700

    delete product

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,8 +1,9 @@
 "use server";
 
-import { LATEST_PRODUCTS_LIMIT, PAGE_SIZE } from "@/constants";
-import { convertToPlainObject } from "../utils";
+import { LATEST_PRODUCTS_LIMIT, PAGE_SIZE, ROUTES } from "@/constants";
+import { convertToPlainObject, formatError } from "../utils";
 import { prisma } from "@/db/prisma";
+import { revalidatePath } from "next/cache";
 
 export async function getLatestProduct() {
   const data = await prisma.product.findMany({
@@ -40,4 +41,24 @@ export async function getAllProducts({
     data,
     totalPages: Math.ceil(dataCount / limit),
   };
+}
+
+export async function deleteProduct(id: string) {
+  try {
+    const productExists = await prisma.product.findFirst({
+      where: { id },
+    });
+
+    if (!productExists) {
+      throw new Error("Product not found");
+    }
+    await prisma.product.delete({ where: { id } });
+    revalidatePath(ROUTES.admin.products.base);
+    return {
+      success: true,
+      message: "product deleted successfully",
+    };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
 }
