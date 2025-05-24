9dd6057872cac3ea5459ce5407c590b6b1c73e45
anhdt <tuananh92.ftu@gmail.com>
2025-05-24 19:27:15 +0700

    add stripe webhook for payment success

---
+++ app/api/webhooks/stripe/route.ts
@@ -0,0 +1,35 @@
+import { updateOrderToPaid } from "@/lib/actions/order.actions";
+import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+
+export async function POST(req: NextRequest) {
+  const event = await stripe.webhooks.constructEventAsync(
+    await req.text(),
+    req.headers.get("stripe-signature") as string,
+    process.env.STRIPE_WEBHOOK_SECRET as string,
+  );
+  if ((event.type = "charge.succeeded")) {
+    const object: any = event.data.object;
+
+    await updateOrderToPaid({
+      orderId: object.metadata.orderId,
+      paymentResult: {
+        id: object.id,
+        status: "COMPLETED",
+        email_address: object.billing_details.email!,
+
+        pricePaid: (object.amount / 100).toFixed(),
+      },
+    });
+
+    return NextResponse.json({
+      message: "updateOrderToPaid was successful",
+    });
+  }
+
+  return NextResponse.json({
+    message: "event is not charge.succeeded",
+  });
+}
