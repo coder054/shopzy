a5f5d0a03bebbb116cb871688cb6833ce9646ede
anhdt <tuananh92.ftu@gmail.com>
2025-05-24 20:44:27 +0700

    receipt email template

---
+++ email/index.tsx
@@ -0,0 +1,15 @@
+import { APP_NAME, SENDER_EMAIL } from "@/constants";
+import { Order } from "@/types";
+import { Resend } from "resend";
+import PurchaseReiceptEmail from "./purchase-receipt";
+
+const resend = new Resend(process.env.RESEND_API_KEY as string);
+
+export const sendPurchaseReceipt = async ({ order }: { order: Order }) => {
+  await resend.emails.send({
+    from: `${APP_NAME} <${SENDER_EMAIL}>`,
+    subject: `Order confirmation ${order.id}`,
+    to: order.user.email,
+    react: <PurchaseReiceptEmail order={order} />,
+  });
+};
